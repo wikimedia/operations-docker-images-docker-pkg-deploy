e9b9146f15d623253f05a98b8123711d7aa54c33
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2017-10-30 13:35:49 +0100

    Various fixes
    * Drop CI from the server_groups for now
    * fix path of the cmd.sh script
    * we don't use git-fat anymore
    * Makefile.deploy : use --never-download for the virtualenv
    * Do not try to link to /usr/local/sbin
    
    Change-Id: Icdd7250c9b4833f48bedf4d3745f9985b7e88ca4

--- scap/cmd.sh
+++ scap/cmd.sh
@@ -2,4 +2,3 @@
 set -eu
 export VENV="/srv/deployment/docker-pkg/venv"
 (cd /srv/deployment/docker-pkg/deploy && make -f Makefile.deploy deploy)
-ln -s "${VENV}/bin/docker-pkg" /usr/local/sbin/docker-pkg
